02cdcc745b2ad8ce58297203473e720444075fc9
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-29 21:03:34 +0100

    navbar, sidebar and footer complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Navbar.css'
+
+function Navbar() {
+  return (
+  
+    <nav className="navbar">
+      <img src="./src/assets/to-do-icon.jpg" alt="app-logo"></img>
+      <h1>MY TASKS</h1>
+      {/* <p>Profile</p>
+      <p>Settings</p>
+      <p>Log out</p> */}
+      </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+  
+    <div class="sidebar">
+      <p>Profile</p>
+      <p>Settings</p>
+      <p>Log out</p>
+      </div>
+  );
+}
+
+
+export default Sidebar;
